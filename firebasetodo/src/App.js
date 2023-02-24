04232ec5519ba83dacc7a205f5ec57984158cd95
Aku Koskela <Aku.Koskela@student.lab.fi>
2023-02-24 17:00:10 +0200

    toimii vielä

--- firebasetodo/src/App.js
+++ firebasetodo/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { AgGridColumn, AgGridReact } from 'ag-grid-react';
-import { DataGrid } from '@mui/x-data-grid';
+import { AgGridReact } from 'ag-grid-react';
 
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-material.css';
@@ -18,13 +17,26 @@ function App() {
       .then(response => response.json())
       .then(data => setTodos(Object.values(data)))
       .catch(err => console.error(err))
-  }
 
- 
+  }
+  const rowData = todos
+
+  const columnDefs = [
+    { headerName: 'Description', field: "description" },
+    { headerName: 'Date', field: "date" },
+    { headerName: 'Priority', field: "priority" }
+  ]
+
 
   return (
     <div className="App">
 
+      <div className="ag-theme-material" style={{ height: 600, width: 600, margin: 'auto' }}>
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={columnDefs}>
+        </AgGridReact>
+      </div>
 
     </div>
   );
